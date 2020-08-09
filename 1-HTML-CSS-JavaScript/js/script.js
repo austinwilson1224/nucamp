ed6bed0dfee2221eff8a1c3609f82c1f1501344f
austinwilson1224 <austin1224@gmail.com>
2020-08-09 10:15:53 -0700

    js

---
+++ 1-HTML-CSS-JavaScript/js/script.js
@@ -0,0 +1,15 @@
+// alert("hello world!");
+
+// errrooror
+console.log("Hello, world!");
+
+function getName() {
+    const userName = prompt("what is you name?");
+    helloWorld(userName);
+}
+
+function helloWorld(userName) {
+    // const userName = prompt("what is your name?")
+    alert("Hello " + userName + "!");
+
+}
