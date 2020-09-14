e7bd07a612bce66fe8e2034ea1b620600d4b9e8b
austinwilson1224 <austin1224@gmail.com>
2020-09-14 16:10:32 -0700

    npm and sass'

---
+++ 2-Bootstrap/nucampsite/js/scripts.js
@@ -0,0 +1,12 @@
+$(".carousel").carousel( { interval: 2000 } ); 
+$("#carouselButton").click(function() {
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+        $("carousel").carousel("pause");
+        $("#carouselButton").children("i").removeClass("fa-pause");
+        $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+        $("carousel").carousel("play");
+        $("#carouselButton").children("i").removeClass("fa-play");
+        $("#carouselButton").children("i").addClass("fa-pause");
+    }
+});
