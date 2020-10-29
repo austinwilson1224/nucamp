4f9e563b41d82f324ccb66122cc64dab6ae89f0c
austinwilson1224 <austin1224@gmail.com>
2020-10-29 16:59:14 -0700

    react  stuff

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -2,88 +2,87 @@ import React from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
 
 
-export default class  CampsiteInfoComponent extends React.Component {
 
+function RenderCampsite({campsite}) {
 
+    return (
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
 
-    // in class  based components don't use let or const keyword 
-    renderCampsite = (campsite) =>{
+function RenderComments ({comments}) {
+    // check if comments are not null
+    // comments != null || comments === undefined
+    if(comments) {
         return (
             <div className="col-md-5 m-1">
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
-        );
-    }
-
-    renderComments (comments) {
-        // check if comments are not null
-        // comments != null || comments === undefined
-        if(comments) {
-            return (
-                <div className="col-md-5 m-1">
-                    <h4>Comments</h4>
-                    
-                    <div>
-                        {comments.map(comment =>{
-                            return (
-                                <div id={comment.id}>
-
-
-                                    {/* should we use span or p here */}
-                                    <p>{comment.text}
-                                    <br/>
-                                    {`--${comment.author}, 
-                                        ${new Intl.DateTimeFormat('en-US', 
-                                        { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}`}
-                                    </p>
-
-
-                                    {/* <span>{comment.author}{new Intl.DateTimeFormat('en-US', 
-                                        { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                                    </span> */}
-                                </div>
-                            )
-                            //`${comment.text}`+ `\n\n${comment.author}, ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}`
-                        })}
-                    </div>
+                <h4>Comments</h4>
                 
+                <div>
+                    {comments.map(comment =>{
+                        return (
+                            <div id={comment.id}>
+
+
+                                {/* should we use span or p here */}
+                                <p>{comment.text}
+                                <br/>
+                                {`--${comment.author}, 
+                                    ${new Intl.DateTimeFormat('en-US', 
+                                    { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}`}
+                                </p>
+
+
+                                {/* <span>{comment.author}{new Intl.DateTimeFormat('en-US', 
+                                    { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                </span> */}
+                            </div>
+                        )
+                        //`${comment.text}`+ `\n\n${comment.author}, ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}`
+                    })}
                 </div>
-            )
-        }
-        return (<div />)
+            
+            </div>
+        )
     }
+    return (<div />)
+}
 
     
 
 
 
 
-    render() {
-        console.log(this.props);
-        if(this.props.campsite) {
-            return (
-
-                <div className="container">
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
-                </div>
-                </div>
-
-            )
-        }
+function CampsiteInfo (props) {
+    // console.log(this.props);
+    if(props.campsite) {
         return (
-            <div></div>
-        )
 
+            <div className="container">
+            <div className="row">
+                <RenderCampsite campsite={props.campsite} />
+                <RenderComments comments={props.campsite.comments} />
+            </div>
+            </div>
+
+        )
     }
+    return (
+        <div></div>
+    )
+
 }
+
+
+export default CampsiteInfo;
 
 
 
