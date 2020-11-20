4a8c504ffd05a89fb745dad51e888107331400d0
austinwilson1224 <austin1224@gmail.com>
2020-11-20 02:27:04 -0800

    started workshop

---
+++ 4-React-Native/nucampsite/components/ContactComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+
+
+
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    static navigationOptions = {
+        title: "Contact"
+    }
+
+    render() {
+        return(
+            <ScrollView>
+
+            </ScrollView>
+        )
+    }
+}
+
+export default Contact;
